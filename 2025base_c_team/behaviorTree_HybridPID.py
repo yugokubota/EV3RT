3bada072ff92ce6e1100270139c65021463327f2
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-06-27 19:34:52 +0900

    feature_kubota_ダブルループ制御の追加

--- 2025base_c_team/behaviorTree_HybridPID.py
+++ 2025base_c_team/behaviorTree_HybridPID.py
@@ -450,8 +450,28 @@ def build_behaviour_tree() -> BehaviourTree:
     avoid_seq = Sequence(name="avoid_seq", memory=True, children=[
         IsObstacleNear(name="obstacle?"),
         AvoidObstacleArcFull(name="arc avoid")
-    junction_seq = Sequence(name="junction_seq", memory=True)
-    
+    ])
+    enter_circle = Sequence(name="enter_circle", memory=True)
+    enter_circle.add_children([
+        IsJunction(name="enter_junction", target_state=JState.FORKING),
+        ArcTurn(name="arc_into_circle", direction="right", degree=90, power=30, radius=200)
+    ])
+    double_loop = Sequence(name="eight_loop", memory=True)
+    double_loop.add_children([
+        TraceLine_sensor(name="trace_outer"),
+        IsJunction(name="cross_junction1", target_state=JState.JOINING),
+        ArcTurn(name="arc_to_small", direction="left", degree=90, power=30, radius=80),
+        TraceLine_sensor(name="trace_inner"),
+        IsJunction(name="cross_junction2", target_state=JState.FORKING),
+        ArcTurn(name="arc_to_big", direction="right", degree=90, power=30, radius=200)
+    ])
+    mid_selector = Selector(name="mid_selector", memory=True)
+    mid_selector.add_children([
+        Sequence(name="loop_seq", memory=True, children=[
+            enter_circle,
+            double_loop,
+        ]),
+        TraceLine_sensor(name="normal_trace")
     ])
 
     traceline_cam = TraceLineCam(
@@ -472,7 +492,7 @@ def build_behaviour_tree() -> BehaviourTree:
     loop_01 = Sequence(name="loop_01_with_obstacle", memory=True)
     loop_01.add_children([
         obstacle_handler,# 20250625_add_kubota_オブジェクト回避のノード追加
-        junction_seq,#20250627_add_kubota_ダブルループ分岐の検出
+        mid_selector,#20250627_add_kubota_ダブルループの制御
         IsDistanceEarned(name="check distance", delta_dist=40000)
     ])
 
