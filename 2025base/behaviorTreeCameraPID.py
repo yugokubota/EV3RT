46840702db7669aa60d1747744f122714e99940c
MarinoMachida <marino-machida@ms-ins.com>
2025-06-30 19:56:38 +0900

    曲がる距離400㎜

--- 2025base/behaviorTreeCameraPID.py
+++ 2025base/behaviorTreeCameraPID.py
@@ -427,7 +427,7 @@ def build_behaviour_tree() -> BehaviourTree:
     print("--くの字用")
     obstacle_sequence.add_children(
         [
-            IsSonarOn(name="check obstacle", alert_dist=150),
+            IsSonarOn(name="check obstacle", alert_dist=400),
             AvoidKShape(name="avoid K-shape path")
         ]
     )
