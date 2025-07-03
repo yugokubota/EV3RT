13e7198417f331cdec5b24d04ebec97df7d174fc
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-07-03 20:50:37 +0900

    kubo

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -408,22 +408,22 @@ class AvoidObstacleArcFull(Behaviour):
         # 右カーブ
         g_left_motor.set_power(60)
         g_right_motor.set_power(20)
-        time.sleep(1)  # 必要に応じて調整
+        time.sleep(1.25)  # 必要に応じて調整
         # 止める
         g_left_motor.set_power(0)
         g_right_motor.set_power(0)
 
         # 左に戻す
-        g_left_motor.set_power(30)
-        g_right_motor.set_power(50)
+        g_left_motor.set_power(20)
+        g_right_motor.set_power(60)
         time.sleep(0.5)  # 必要に応じて調整
         g_left_motor.set_power(0)
         g_right_motor.set_power(0)
 
         # 左に戻してライン復帰
-        g_left_motor.set_power(40)
+        g_left_motor.set_power(20)
         g_right_motor.set_power(50)
-        time.sleep(1.5)
+        time.sleep(1)
         g_left_motor.set_power(0)
         g_right_motor.set_power(0)
 
