811ed57dff6efc83fd86f825340c68a745bc1d1e
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-07-03 19:15:43 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -370,7 +370,7 @@ class VideoThread(threading.Thread):
             time.sleep(VIDEO_INTERVAL)
 
 class IsObstacleNear(Behaviour):# 20250625_add_kubota_ソナーで障害物検知するクラス追加
-    def __init__(self, name: str, threshold: int = 300):
+    def __init__(self, name: str, threshold: int = 500):
         super().__init__(name)
         self.threshold = threshold
 
@@ -407,7 +407,7 @@ class AvoidObstacleArcFull(Behaviour):
         # 右カーブ
         g_left_motor.set_power(50)
         g_right_motor.set_power(10)
-        time.sleep(2)  # 必要に応じて調整
+        time.sleep(3)  # 必要に応じて調整
         # 止める
         g_left_motor.set_power(0)
         g_right_motor.set_power(0)
