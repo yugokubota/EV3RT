685040379c71e23625985e5d62d7a97ea3a600a1
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-07-04 17:57:15 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -420,12 +420,12 @@ class AvoidObstacleArcFull(Behaviour):
         g_left_motor.set_power(0)
         g_right_motor.set_power(0)
 
-        # # 左に戻してライン復帰
-        # g_left_motor.set_power(20)
-        # g_right_motor.set_power(40)
-        # time.sleep(1)
-        # g_left_motor.set_power(0)
-        # g_right_motor.set_power(0)
+        # ライン復帰
+        g_left_motor.set_power(40)
+        g_right_motor.set_power(30)
+        time.sleep(2)
+        g_left_motor.set_power(0)
+        g_right_motor.set_power(0)
 
         # フラグを立てて終了
         self.done = True
@@ -542,8 +542,8 @@ def build_behaviour_tree() -> BehaviourTree:
     loop_01.add_children([
         obstacle_selector,
         # mid_selector,
-        TraceLineCam(name="trace_clear_obstacle",power=40, pid_p=2.0, pid_i=0.0012, pid_d=0.18,
-        gs_min=0, gs_max=80,trace_side=TraceSide.NORMAL),
+        # TraceLineCam(name="trace_clear_obstacle",power=40, pid_p=2.0, pid_i=0.0012, pid_d=0.18,
+        # gs_min=0, gs_max=80,trace_side=TraceSide.NORMAL),
         # IsDistanceEarned(name="check distance", delta_dist=40000)
     ])
     calibration = Sequence(name="calibration", memory=True)
