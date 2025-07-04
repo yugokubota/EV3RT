17520277f13fcb31f38f7fb90d6b52416776d6c3
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-07-04 17:43:25 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -416,7 +416,7 @@ class AvoidObstacleArcFull(Behaviour):
         # 左に戻す
         g_left_motor.set_power(20)
         g_right_motor.set_power(60)
-        time.sleep(1)  # 必要に応じて調整
+        time.sleep(1.5)  # 必要に応じて調整
         g_left_motor.set_power(0)
         g_right_motor.set_power(0)
 
@@ -488,7 +488,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # オブジェクトを回避するためのノード
     avoid_seq = Sequence(name="avoid_seq", memory=True)
     avoid_seq.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=2650),
+        IsDistancePassed(name="distance_passed", target_distance=2700),
         AvoidObstacleArcFull(name="arc_avoid")
     ])
     # オブジェクト回避のライントレース
