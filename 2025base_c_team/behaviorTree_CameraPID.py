ce419802484361dda6b473fb87e9f7e6c75a6346
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-07-07 18:49:07 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -501,7 +501,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # オブジェクト回避のライントレース
     traceline_cam_for_obstacle = TraceLineCam(
         name="camera_trace_for_obstacle",
-        power=55, pid_p=2.4, pid_i=0.002, pid_d=0.25,
+        power=65, pid_p=2.4, pid_i=0.002, pid_d=0.25,
         gs_min=0, gs_max=40,
         trace_side=TraceSide.NORMAL
     )
