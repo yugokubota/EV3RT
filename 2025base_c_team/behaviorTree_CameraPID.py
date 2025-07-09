4c0098b492f08e3a604e4d80f41b1fd7cea3e14e
honda <daahon129@gmail.com>
2025-07-09 19:24:17 +0900

    p=1.4 d=0.6 gs=10~80 ふらつき＆急旋回対策版

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -501,8 +501,8 @@ def build_behaviour_tree() -> BehaviourTree:
     # オブジェクト回避のライントレース
     traceline_cam_for_obstacle = TraceLineCam(
         name="camera_trace_for_obstacle",
-        power=60, pid_p=1.6, pid_i=0, pid_d=0.5,
-        gs_min=0, gs_max=40,
+        power=60, pid_p=1.4, pid_i=0, pid_d=0.6,
+        gs_min=10, gs_max=80,
         trace_side=TraceSide.NORMAL
     )
     # ダブルループのライントレース
