6e32b171ecae6c2938d7f2a7575e41e81d1bff38
honda <daahon129@gmail.com>
2025-07-10 18:54:43 +0900

    カーブ調整

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -438,7 +438,7 @@ class AvoidObstacleArcFull(Behaviour):
         # 右半円カーブ（ゆっくり穏やかに）
         g_left_motor.set_power(100)    # 左モーター少し強め
         g_right_motor.set_power(55)   # 右モーターも上げて差を縮小
-        time.sleep(1.0)               # 時間は微調整してください
+        time.sleep(0.5)               # 時間は微調整してください
 
         # 止める（少し余韻を持たせて）
         g_left_motor.set_power(0)
@@ -446,8 +446,8 @@ class AvoidObstacleArcFull(Behaviour):
         time.sleep(0.3)
 
         # 左に戻す（半円を描く後半）
-        g_left_motor.set_power(50)
-        g_right_motor.set_power(100)
+        g_left_motor.set_power(100)
+        g_right_motor.set_power(200)
         time.sleep(1.0)
 
         g_left_motor.set_power(0)
