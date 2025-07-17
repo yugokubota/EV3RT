bdb2a8c847d5932677f30d4e06673976f0f0d9db
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-07-17 21:16:02 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -684,8 +684,8 @@ def build_behaviour_tree() -> BehaviourTree:
     # part1_黒線を検知した場合ライントレース
     distance_loop_Parallel = Parallel(name="distance_loop_Parallel", policy=ParallelPolicy.SuccessOnOne())
     distance_loop_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=1000),
-        RunAsInstructed(name="go_straight_1", pwm_l=57, pwm_r=50),
+        IsDistancePassed(name="distance_passed", target_distance=900),
+        RunAsInstructed(name="go_straight_1", pwm_l=54, pwm_r=50),
     ])
     # part1_黒線を検知した場合ライントレース
     double_loop_black_selector_1 = Selector(name="double_loop_black_selector1",memory=False)
