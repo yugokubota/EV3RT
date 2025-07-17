7673a87f812c19099c6e051b485d5097542dc83b
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-07-17 20:11:43 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -620,8 +620,8 @@ def build_behaviour_tree() -> BehaviourTree:
     traceline_cam_lapfinish_Parallel = Parallel(name="detectblue_or_trace", policy=ParallelPolicy.SuccessOnOne())
     traceline_cam_lapfinish_Parallel.add_children([
         DetectBlue(name="detect_blue"),
-        TraceLineCam(name="traceline_cam_lapfinish",power=50, pid_p=1.4, pid_i=0.001, pid_d=0.12,
-        gs_min=0, gs_max=60,trace_side=TraceSide.CENTER),
+        TraceLineCam(name="traceline_cam_lapfinish",power=50, pid_p=2.0, pid_i=0.0012, pid_d=0.18,
+        gs_min=0, gs_max=80,trace_side=TraceSide.CENTER),
     ])
     # ================ ダブルループ処理 ================
 
