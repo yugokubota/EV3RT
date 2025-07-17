922bc000fc512ba7308b09607c7b97cfe778cc8a
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-07-17 20:01:19 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -621,7 +621,7 @@ def build_behaviour_tree() -> BehaviourTree:
     traceline_cam_lapfinish_Parallel.add_children([
         DetectBlue(name="detect_blue"),
         TraceLineCam(name="traceline_cam_lapfinish",power=50, pid_p=1.4, pid_i=0.001, pid_d=0.12,
-        gs_min=0, gs_max=60,trace_side=TraceSide.NORMAL),
+        gs_min=0, gs_max=60,trace_side=TraceSide.CENTER),
     ])
     # ================ ダブルループ処理 ================
 
