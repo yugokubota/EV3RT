ee62ca6e70f60d005fc7b806009a96a305c0361c
honda <daahon129@gmail.com>
2025-07-18 19:27:05 +0900

    left motor 85

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -406,7 +406,7 @@ class AvoidObstacleArcFull(Behaviour):
 
         # --- 以下、単純な回避動作 ---
         # 右カーブ
-        g_left_motor.set_power(80)
+        g_left_motor.set_power(85)
         g_right_motor.set_power(40)
         time.sleep(1)  # 必要に応じて調整
         # 止める
