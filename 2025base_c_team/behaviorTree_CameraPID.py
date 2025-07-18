540b30e1010d4c0998993d60f01ad425f473b1f5
honda <daahon129@gmail.com>
2025-07-18 19:38:30 +0900

    distance modosu

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -495,8 +495,8 @@ def build_behaviour_tree() -> BehaviourTree:
     # オブジェクトを回避するためのノード
     avoid_seq = Sequence(name="avoid_seq", memory=True)
     avoid_seq.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=1),
-        # IsDistancePassed(name="distance_passed", target_distance=2700),
+        # IsDistancePassed(name="distance_passed", target_distance=1),
+        IsDistancePassed(name="distance_passed", target_distance=2700),
         AvoidObstacleArcFull(name="arc_avoid")
     ])
     # オブジェクト回避のライントレース
