6eab2340f53ae8257051330e888b24e1364439aa
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-07-20 16:05:58 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -647,14 +647,14 @@ def build_behaviour_tree() -> BehaviourTree:
     double_loop_black_selector_2.add_children([
         IsOnBlackLine(name="detect_blackline_2", threshold=5),
         # RunAsInstructed(name="go_straight_2", pwm_l=45, pwm_r=48),
-        RunAsInstructed(name="go_straight_1", pwm_l=-45, pwm_r=-40),#RIGHT用
+        RunAsInstructed(name="go_straight_2", pwm_l=-40, pwm_r=-45),#RIGHT用
     ])
     # part3_黒線を検知した場合ライントレース
     double_loop_black_selector_3 = Selector(name="double_loop_black_selector3",memory=False)
     double_loop_black_selector_3.add_children([
         IsOnBlackLine(name="detect_blackline_3", threshold=5),
         # RunAsInstructed(name="go_straight_3", pwm_l=40, pwm_r=47),
-        RunAsInstructed(name="go_straight_1", pwm_l=-47, pwm_r=-40),#RIGHT用
+        RunAsInstructed(name="go_straight_3", pwm_l=-47, pwm_r=-40),#RIGHT用
     ])
     # part4_黒線を検知した場合ライントレース
     double_loop_black_selector_4 = Selector(name="double_loop_black_selector4",memory=False)
