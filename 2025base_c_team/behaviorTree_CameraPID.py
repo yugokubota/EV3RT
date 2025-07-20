1d60991985a49cada2db46d121e80917d07b0356
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-07-20 16:22:12 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -690,7 +690,7 @@ def build_behaviour_tree() -> BehaviourTree:
 
     loop_01 = Sequence(name="loop_01_with_obstacle_and_doubleloop", memory=True)
     loop_01.add_children([
-        # Detectcolor(name="detectcolor"),#       色や明るさを検知できる
+        Detectcolor(name="detectcolor"),#       色や明るさを検知できる
         # --------直線とオブジェクト回避--------
         # obstacle_Parallel,#                     直線のライントレースをする。一定距離走ったらオブジェクト回避して抜ける。
         traceline_cam_lapfinish_Parallel,#        オブジェクト回避後からLAP通過までのライントレース（青いライン検知で抜ける）
