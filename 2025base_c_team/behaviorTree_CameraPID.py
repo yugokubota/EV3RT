4c430df38d4d9a5a1ffbceef1f630e8ec6de9c07
MarinoMachida <marino-machida@ms-ins.com>
2025-07-23 22:02:21 +0900

    name="go_straight_3", pwm_l=-50, pwm_r=-40

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -653,7 +653,7 @@ def build_behaviour_tree() -> BehaviourTree:
     double_loop_black_selector_3.add_children([
         IsOnBlackLine(name="detect_blackline_3", threshold=5),
         #RunAsInstructed(name="go_straight_3", pwm_l=40, pwm_r=47),      #LEFT用
-        RunAsInstructed(name="go_straight_3", pwm_l=-47, pwm_r=-40),  #RIGHT用
+        RunAsInstructed(name="go_straight_3", pwm_l=-50, pwm_r=-40),  #RIGHT用
     ])
     # part4_黒線を検知した場合ライントレース
     double_loop_black_selector_4 = Selector(name="double_loop_black_selector4",memory=False)
@@ -674,7 +674,7 @@ def build_behaviour_tree() -> BehaviourTree:
     BigCircleEntryTuning_selector.add_children([
         IsDistancePassed(name="distance_passed", target_distance=250),  #200は適当なので要調整
         #RunAsInstructed(name="BigCircle_Entry", pwm_l=60, pwm_r=50),      #LEFT用
-        RunAsInstructed(name="BigCircle_Entry", pwm_l=-50, pwm_r=-62),  #RIGHT用
+        RunAsInstructed(name="BigCircle_Entry", pwm_l=-50, pwm_r=-60),  #RIGHT用
     ])
 
     # ================ 青色検知するまでライントレース ================
