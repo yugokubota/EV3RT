46469673b90d4b54f8c195dcd1d9d4f1405eb636
shinba4811 <shinba4811shinba4811@gmail.com>
2025-07-30 19:06:33 +0900

    tyousei

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -521,7 +521,7 @@ class AvoidObstacleArcFull(Behaviour):
         # 右カーブ
         g_left_motor.set_power(100)
         g_right_motor.set_power(60)
-        time.sleep(0.7)  # 必要に応じて調整
+        time.sleep(0.6)  # 必要に応じて調整
         # 止める
         g_left_motor.set_power(0)
         g_right_motor.set_power(0)
@@ -529,14 +529,14 @@ class AvoidObstacleArcFull(Behaviour):
         # 左に戻す
         g_left_motor.set_power(60)
         g_right_motor.set_power(100)
-        time.sleep(0.95)  # 必要に応じて調整 
+        time.sleep(0.9)  # 必要に応じて調整 
         #g_left_motor.set_power(0)
         #g_right_motor.set_power(0)
 
         # ライン復帰
         g_left_motor.set_power(100)
         g_right_motor.set_power(60)
-        time.sleep(0.45)
+        time.sleep(0.4)
         g_left_motor.set_power(0)
         g_right_motor.set_power(0)
 
