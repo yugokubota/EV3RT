f2d830d8310c422f50da4df51d68440051ff0e47
honda <daahon129@gmail.com>
2025-08-01 20:15:27 +0900

    632行目　power=70, pid_p=0.4, pid_i=0.002, pid_d=0.3,

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -629,7 +629,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # オブジェクト回避前のライントレース
     traceline_cam_for_obstacle = TraceLineCam(
         name="camera_trace_for_obstacle",
-        power=90, pid_p=0.4, pid_i=0.002, pid_d=0.3,
+        power=70, pid_p=0.4, pid_i=0.002, pid_d=0.3,
         gs_min=0, gs_max=40,
         trace_side=TraceSide.NORMAL
     )
