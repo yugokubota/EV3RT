3d6fb921c4bce503a9149e4d66e152fa9ce8e602
MarinoMachida <marino-machida@ms-ins.com>
2025-08-07 20:19:08 +0900

    target_distance=550

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -672,7 +672,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # 大円に入るときの調整
     BigCircleEntryTuning_selector = Parallel(name="BigCircleEntryTuning_selector", policy=ParallelPolicy.SuccessOnOne())
     BigCircleEntryTuning_selector.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=500),  #200は適当なので要調整
+        IsDistancePassed(name="distance_passed", target_distance=550),  #200は適当なので要調整
         #RunAsInstructed(name="BigCircle_Entry", pwm_l=60, pwm_r=50),      #LEFT用
         RunAsInstructed(name="BigCircle_Entry", pwm_l=-60, pwm_r=-50),  #RIGHT用
     ])
