b97c53989e2f66499033af026427cc2f3d9a2e7a
YasuyukiOda <MSADsystems6M748480@outlook.jp>
2025-08-08 19:46:37 +0900

    pwm-54に変更

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -632,7 +632,7 @@ def build_behaviour_tree() -> BehaviourTree:
     distance_loop_Parallel.add_children([
         IsDistancePassed(name="distance_passed", target_distance=800),
         #RunAsInstructed(name="go_straight", pwm_l=58, pwm_r=50),      #LEFT用
-        RunAsInstructed(name="go_straight", pwm_l=-50, pwm_r=-58),  #RIGHT用
+        RunAsInstructed(name="go_straight", pwm_l=-50, pwm_r=-54),  #RIGHT用
     ])
     # part1_黒線を検知した場合ライントレース
     double_loop_black_selector_1 = Selector(name="double_loop_black_selector1",memory=False)
