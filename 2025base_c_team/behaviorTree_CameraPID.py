68473eb1be91a42bf2dae2e185aa27a6d8d87edc
shinba4811 <shinba4811shinba4811@gmail.com>
2025-08-17 16:28:33 +0900

    tyousei

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -627,7 +627,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # オブジェクト回避前のライントレース
     traceline_cam_for_obstacle = TraceLineCam(
         name="camera_trace_for_obstacle",
-        power=50, pid_p=1.4, pid_i=0.0015, pid_d=0.3,
+        power=60, pid_p=0.4, pid_i=0.0006, pid_d=0.0.06,
         #power=60, pid_p=1.75, pid_i=0.0012, pid_d=0.18,
         gs_min=0, gs_max=40,
         trace_side=TraceSide.NORMAL
