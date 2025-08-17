7e090ae8a52d1fc2f38e0db82626c537e7abc729
shinba4811 <shinba4811shinba4811@gmail.com>
2025-08-17 15:28:16 +0900

    CENTER

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -629,7 +629,7 @@ def build_behaviour_tree() -> BehaviourTree:
         name="camera_trace_for_obstacle",
         power=60, pid_p=1.4, pid_i=0.0015, pid_d=0.25,
         gs_min=0, gs_max=40,
-        trace_side=TraceSide.NORMAL
+        trace_side=TraceSide.CENTER
     )
     # オブジェクト回避とライントレース
     obstacle_Parallel = Parallel(name="obstacle_or_trace", policy=ParallelPolicy.SuccessOnOne())
