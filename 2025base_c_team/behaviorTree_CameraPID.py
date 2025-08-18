a255021c9774796fa53ceeab8a337c9c7f5e0524
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-08-18 20:08:40 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -929,7 +929,7 @@ def build_behaviour_tree() -> BehaviourTree:
     After_puton_back_first_Parallel = Parallel(name="After_puton_back", policy=ParallelPolicy.SuccessOnOne())
     After_puton_back_first_Parallel.add_children([
         IsDistancePassed(name="distance_passed_back", target_distance=300),
-        RunByGyro(name="run_back_After_puton_back_first", target=0, power=60,
+        RunByGyro(name="run_back_After_puton_back_first", target=30, power=60,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
         # RunByGyro(name="run_back_After_puton_back_first", target=180, power=-60,
         #         pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
