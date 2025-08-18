a1ddcc7b20bc75faad28f57824dc1b692ee79d2f
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-08-18 20:02:27 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -901,7 +901,7 @@ def build_behaviour_tree() -> BehaviourTree:
     BringObject_to_Gate_Parallel = Parallel(name="BringObject_to_Gate", policy=ParallelPolicy.SuccessOnOne())
     BringObject_to_Gate_Parallel.add_children([
         # -----ゲートの位置で距離が変わるようになっている⇒gate_value(300=front, 500=back)
-        IsDistancePassed(name="distance_passed", target_distance=gate_value(500, 700)),
+        IsDistancePassed(name="distance_passed", target_distance=gate_value(550, 750)),
         RunByGyro(name="run straight_BringObject_to_Gate", target=0, power=60,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
@@ -929,7 +929,7 @@ def build_behaviour_tree() -> BehaviourTree:
     After_puton_back_first_Parallel = Parallel(name="After_puton_back", policy=ParallelPolicy.SuccessOnOne())
     After_puton_back_first_Parallel.add_children([
         IsDistancePassed(name="distance_passed_back", target_distance=300),
-        RunByGyro(name="run_back_After_puton_back_first", target=-135, power=-60,
+        RunByGyro(name="run_back_After_puton_back_first", target=-45, power=60,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
         # RunByGyro(name="run_back_After_puton_back_first", target=180, power=-60,
         #         pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
