aafc94526c3d46ffa3fc5e5e2a738ec2258dd3d2
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-08-18 19:20:23 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -976,10 +976,10 @@ def build_behaviour_tree() -> BehaviourTree:
     SpinAndRun_Sequence = Sequence(name="SpinAndRun_Sequence", memory=True)
     SpinAndRun_Sequence.add_children([
         BringObject_to_Gate_Parallel,#-----ゲート位置までオブジェクトを運ぶ（ゲート位置によって距離制御あり）
-        SpinAround(name="spin by 90 degrees_SpinAndRun_1", target=90, max_power=60, min_power=MIN_POWER,
-                    pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
-        # SpinAround(name="spin by 90 degrees_SpinAndRun_1", target=-90, max_power=60, min_power=MIN_POWER,
+        # SpinAround(name="spin by 90 degrees_SpinAndRun_1", target=90, max_power=60, min_power=MIN_POWER,
         #             pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
+        SpinAround(name="spin by 90 degrees_SpinAndRun_1", target=-90, max_power=60, min_power=MIN_POWER,
+                    pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
         # SpinAround(name="spin by 90 degrees", target=90, max_power=45, min_power=MIN_POWER,
         #             pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
         SpinAndRun_Parallel,#--------------ゲートを通過する（ゲート位置によって距離制御あり）
