84ab84e61451d5b6712f8d24762bb9133402eda6
T72-Nakajima <naka722.fiena@gmail.com>
2025-08-22 16:29:23 +0900

    RunByGyroのターゲットを-110から-90に変更し、IsDistancePassedを追加

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -979,7 +979,8 @@ def build_behaviour_tree() -> BehaviourTree:
     DetectBlackLine_Parallel = Parallel(name="DetectBlackLine", policy=ParallelPolicy.SuccessOnOne())
     DetectBlackLine_Parallel.add_children([
         IsOnBlackLine(name="detect_blackline", threshold=5),
-        RunByGyro(name="run_back_DetectBlackLine", target=-110, power=40,
+        IsDistancePassed(name="distance_passed_GoBlackLine", target_distance=800),
+        RunByGyro(name="run_back_DetectBlackLine", target=-90, power=40,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
     # --------ジャイロで90°回転して青ライン検知するまでライントレース
