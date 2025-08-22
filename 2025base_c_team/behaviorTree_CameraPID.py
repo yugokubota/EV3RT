bff54240cb9e0159bfe64b9d3f8bf3f50ebc0f89
T72-Nakajima <naka722.fiena@gmail.com>
2025-08-22 16:53:28 +0900

    IsOnBlackLineの実装を更新し、IsOnBlackLine_runningクラスを追加

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -553,6 +553,23 @@ class IsOnBlackLine(Behaviour):#黒色を明るさで検知
         else:
             # self.logger.info("%+06d %s.NotDetected..." % (g_plotter.get_distance(), self.__class__.__name__))
             # print(f"[IsOnBlackLine] NotDetected... brightness={brightness}")
+            return Status.FAILURE
+
+class IsOnBlackLine_running(Behaviour):#黒色を明るさで検知
+    def __init__(self, name: str, threshold: int = 40):
+        super().__init__(name)
+        self.threshold = threshold
+        self.logger.debug("%s.__init__()" % (self.__class__.__name__))
+
+    def update(self) -> Status:
+        brightness = g_color_sensor.get_brightness()
+        if brightness < self.threshold:  # 明るさがthreshold未満=黒い
+            self.logger.info("%+06d %s.DetectBlack!" % (g_plotter.get_distance(), self.__class__.__name__))
+            print(f"[IsOnBlackLine] Detected! brightness={brightness}")
+            return Status.SUCCESS
+        else:
+            # self.logger.info("%+06d %s.NotDetected..." % (g_plotter.get_distance(), self.__class__.__name__))
+            # print(f"[IsOnBlackLine] NotDetected... brightness={brightness}")
             return Status.RUNNING
 
 class DetectBlackCount(Behaviour):
@@ -978,7 +995,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # ])
     DetectBlackLine_Parallel = Parallel(name="DetectBlackLine", policy=ParallelPolicy.SuccessOnOne())
     DetectBlackLine_Parallel.add_children([
-        IsOnBlackLine(name="detect_blackline", threshold=5),
+        IsOnBlackLine_running(name="detect_blackline", threshold=5),
         IsDistancePassed(name="distance_passed_GoBlackLine", target_distance=400),
         RunByGyro(name="run_back_DetectBlackLine", target=-90, power=40,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
