3bc8acc732fbd09a311ee323ec5c9d4aad82d835
T72-Nakajima <naka722.fiena@gmail.com>
2025-08-22 16:17:32 +0900

    RunByGyroのターゲットを-90から-110に変更

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -979,7 +979,7 @@ def build_behaviour_tree() -> BehaviourTree:
     DetectBlackLine_Parallel = Parallel(name="DetectBlackLine", policy=ParallelPolicy.SuccessOnOne())
     DetectBlackLine_Parallel.add_children([
         IsOnBlackLine(name="detect_blackline", threshold=5),
-        RunByGyro(name="run_back_DetectBlackLine", target=-90, power=30,
+        RunByGyro(name="run_back_DetectBlackLine", target=-110, power=30,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
     # --------ジャイロで90°回転して青ライン検知するまでライントレース
