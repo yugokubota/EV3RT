81b68839a7722fbfce103cc11ed595507a783a33
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-08-23 17:02:15 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -752,7 +752,7 @@ class AvoidObstacleArcFull(Behaviour):
         # ライン復帰
         g_left_motor.set_power(50)
         g_right_motor.set_power(50)
-        time.sleep(0.5)
+        time.sleep(1)
         # 止める
         #g_left_motor.set_power(0)
         #g_right_motor.set_power(0)
