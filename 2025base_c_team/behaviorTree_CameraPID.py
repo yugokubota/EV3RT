97e17933c6cd3986cec765b174428782d46ab5a9
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-08-26 11:20:23 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -869,7 +869,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # オブジェクトを無視してジャイロで真っ直ぐ
     gyro_obstacle_ignore_Parallel = Parallel(name="gyro_obstacle_ignore", policy=ParallelPolicy.SuccessOnOne())
     gyro_obstacle_ignore_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=4420),#カーブまで
+        IsDistancePassed(name="distance_passed", target_distance=4500),#カーブまで
         RunByGyro(name="run_back_GoBlackLine", target=0, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
