5f68e2a5941336c33293dbf9947cdc66cedea8a8
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-08-29 19:37:15 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -843,7 +843,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # オブジェクトを回避するためのノード
     avoid_seq = Sequence(name="avoid_seq", memory=True)
     avoid_seq.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=2400),
+        IsDistancePassed(name="distance_passed", target_distance=2350),
         SpinAround(name="spin by 90 degrees_After_puton_back_first", target=0, max_power=60, min_power=MIN_POWER,
                     pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
         AvoidObstacleArcFull(name="arc_avoid")
