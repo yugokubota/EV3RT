e29f51315b736a8b2b1a053c320142d63dcbb782
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-08-29 19:23:34 +0900

    kubtoa

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -882,7 +882,7 @@ def build_behaviour_tree() -> BehaviourTree:
     #向正面をジャイロで真っ直ぐ
     gyro_mukoujoumen_Parallel = Parallel(name="gyro_mukoujoumen", policy=ParallelPolicy.SuccessOnOne())
     gyro_mukoujoumen_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=3000),#カーブまで
+        IsDistancePassed(name="distance_passed", target_distance=3050),#カーブまで
         RunByGyro(name="run_back_GoBlackLine", target=-90, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
