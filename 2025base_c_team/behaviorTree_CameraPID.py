b81c4e7a77221372edf3b2b634911e9c6a943f2d
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-08-31 15:59:38 +0900

    kubota

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1035,14 +1035,14 @@ def build_behaviour_tree() -> BehaviourTree:
     SmallCircleEntryTuning_selector.add_children([
         IsDistancePassed(name="distance_passed", target_distance=500),  #200は適当なので要調整
         #RunAsInstructed(name="SmallCircle_Entry", pwm_l=60, pwm_r=50),      #LEFT用
-        RunAsInstructed(name="SmallCircle_Entry", pwm_l=-60, pwm_r=-65),  #RIGHT用
+        RunAsInstructed(name="SmallCircle_Entry", pwm_l=-60, pwm_r=-67),  #RIGHT用
     ])
     # 大円に入るときの調整
     BigCircleEntryTuning_selector = Parallel(name="BigCircleEntryTuning_selector", policy=ParallelPolicy.SuccessOnOne())
     BigCircleEntryTuning_selector.add_children([
         IsDistancePassed(name="distance_passed", target_distance=550),  #200は適当なので要調整
         #RunAsInstructed(name="BigCircle_Entry", pwm_l=60, pwm_r=50),      #LEFT用
-        RunAsInstructed(name="BigCircle_Entry", pwm_l=-60, pwm_r=-50),  #RIGHT用
+        RunAsInstructed(name="BigCircle_Entry", pwm_l=-65, pwm_r=-50),  #RIGHT用
     ])
 
     # ================ 青色検知するまでライントレース ================
