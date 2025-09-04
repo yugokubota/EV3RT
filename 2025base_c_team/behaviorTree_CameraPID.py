12600dddc703f92312c75d904558a9d32deade1a
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-09-04 19:48:19 +0900

    2つ目のボトルを置くところの調整

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1235,7 +1235,7 @@ def build_behaviour_tree() -> BehaviourTree:
     Spintotarget_225degree_Parallel = Parallel(name="Spintotarget_225degree", policy=ParallelPolicy.SuccessOnOne())
     Spintotarget_225degree_Parallel.add_children([
         IsDistancePassed(name="distance_passed_ThroughTheGate", target_distance=50),
-        RunByGyro(name="run straight_SpinAndRun", target=-225, power=70,
+        RunByGyro(name="run straight_SpinAndRun", target=-225, power=60,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
 
@@ -1246,7 +1246,7 @@ def build_behaviour_tree() -> BehaviourTree:
     smart_carry_puton_second_Parallel = Parallel(name="smart_carry_puton", policy=ParallelPolicy.SuccessOnOne())
     smart_carry_puton_second_Parallel.add_children([
         IsDistancePassed(name="smart_carry_puton", target_distance=1600),
-        RunByGyro(name="Gyro_straight_smart_carry_puton_second", target=-270, power=60,
+        RunByGyro(name="Gyro_straight_smart_carry_puton_second", target=-268, power=60,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
 
