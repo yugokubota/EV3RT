525f4d8b9a52e2575fc5d8f9bf25fc990843089a
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-09-04 19:00:35 +0900

    最初のカーブまでの距離を短く

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -961,7 +961,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # [Control] Parallel(SuccessOnOne)
     gyro_obstacle_end_to_first_curve_Parallel = Parallel(name="gyro_obstacle_end_to_first_curve", policy=ParallelPolicy.SuccessOnOne())
     gyro_obstacle_end_to_first_curve_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=800),
+        IsDistancePassed(name="distance_passed", target_distance=750),
         RunByGyro(name="run_back_GoBlackLine", target=0, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
