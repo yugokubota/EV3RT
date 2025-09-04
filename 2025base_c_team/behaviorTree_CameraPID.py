ea89fb364e5fceea2fc1fc4a2a2d1fd827607c9c
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-09-04 19:35:06 +0900

    ダブルループの大円に戻るところの調整

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1099,7 +1099,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # [Control] Parallel(SuccessOnOne)
     BigCircleEntryTuning_Parallel = Parallel(name="BigCircleEntryTuning", policy=ParallelPolicy.SuccessOnOne())
     BigCircleEntryTuning_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=400),
+        IsDistancePassed(name="distance_passed", target_distance=500),
         TraceLineCam(name="traceline_entry_bigcircle",power=42, pid_p=1.75, pid_i=0.0012, pid_d=0.18,
         gs_min=0, gs_max=80,trace_side=TraceSide.NORMAL),
     ])
@@ -1111,7 +1111,7 @@ def build_behaviour_tree() -> BehaviourTree:
     BigCircle_Linetrace_CenterEdge_parallel = Parallel(name="BigCircle_Linetrace_CenterEdge",policy=ParallelPolicy.SuccessOnOne())
     BigCircle_Linetrace_CenterEdge_parallel.add_children([
         DetectBlue(name="detect_blue"),
-        TraceLineCam(name="Tracelinecam_DetectBlue_3",power=42, pid_p=2.0, pid_i=0.0012, pid_d=0.1,
+        TraceLineCam(name="Tracelinecam_DetectBlue_3",power=42, pid_p=1.75, pid_i=0.0012, pid_d=0.18,
         gs_min=0, gs_max=40,trace_side=TraceSide.CENTER),
     ])
 
