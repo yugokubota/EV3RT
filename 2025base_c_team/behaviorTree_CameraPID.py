d1590824586f26ff37d16850307e594eb1830a1f
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-09-08 19:58:57 +0900

    kubota_avoidの値修正

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -794,8 +794,8 @@ class AvoidObstacleArcFull(Behaviour):
         g_left_motor.set_power(0)
         g_right_motor.set_power(0)
         # 右カーブ
-        g_left_motor.set_power(70)
-        g_right_motor.set_power(100)
+        g_left_motor.set_power(100)
+        g_right_motor.set_power(70)
         time.sleep(0.6)  # 必要に応じて調整
         g_left_motor.set_power(80)
         g_right_motor.set_power(100)
