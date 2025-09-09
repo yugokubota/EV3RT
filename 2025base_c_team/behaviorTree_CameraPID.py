5cc1ff0f785986b4573926151d5f6c07148d4eb5
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-09-09 18:30:58 +0900

    定義エラー修正

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1332,7 +1332,7 @@ def build_behaviour_tree() -> BehaviourTree:
         SpinAround(name="Go_to_next_bottle",
                     target=-130,max_power=50,min_power=MIN_POWER,
                     pid_p=1.1,pid_i=0.001,pid_d=0.03,target_type=HeadingType.ABSOLUTE),
-        Spintotarget_130degree_Parallel,
+        Spintotarget_135degree_Parallel,
         # SpinAround(name="spin by 90 degrees_detect_red",
         #             target=-270,max_power=50,min_power=MIN_POWER,
         #             pid_p=1.1,pid_i=0.001,pid_d=0.03,target_type=HeadingType.ABSOLUTE),
