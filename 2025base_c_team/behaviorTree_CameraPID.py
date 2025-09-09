25cc284afc205389e71e1e4ed4176281e284f61a
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-09-09 19:12:32 +0900

    2本目のボトルをつかむ調整

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1351,6 +1351,9 @@ def build_behaviour_tree() -> BehaviourTree:
                     pid_p=1.1,pid_i=0.001,pid_d=0.03,target_type=HeadingType.ABSOLUTE),
         # --- 次のボトルへ
         Go_to_next_bottle_Parallel,
+        SpinAround(name="spin by 90 degrees_After_puton_back_second",
+                    target=-220,max_power=50,min_power=MIN_POWER,
+                    pid_p=1.1,pid_i=0.001,pid_d=0.03,target_type=HeadingType.ABSOLUTE),
         Spintotarget_225degree_Parallel,
         # SpinAround(name="spin by 90 degrees_detect_red",
         #             target=-270,max_power=50,min_power=MIN_POWER,
