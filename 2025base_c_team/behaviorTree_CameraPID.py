95750783854357487649be1d4b78d95865490ef3
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-10-02 18:48:44 +0900

    掴んだあとの距離増 帰りのゲート前まで進む距離増

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1281,7 +1281,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # [Control] Parallel(SuccessOnOne)
     Spintotarget_180degree_Parallel = Parallel(name="Spintotarget_180degree", policy=ParallelPolicy.SuccessOnOne())
     Spintotarget_180degree_Parallel.add_children([
-        IsDistancePassed(name="distance_passed_ThroughTheGate", target_distance=150),
+        IsDistancePassed(name="distance_passed_ThroughTheGate", target_distance=50),
         RunByGyro(name="run straight_SpinAndRun", target=180, power=60,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
@@ -1314,7 +1314,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # [Control] Parallel(SuccessOnOne)
     Spintotarget_360degree_Parallel = Parallel(name="Spintotarget_360degree", policy=ParallelPolicy.SuccessOnOne())
     Spintotarget_360degree_Parallel.add_children([
-        IsDistancePassed(name="distance_passed_ThroughTheGate", target_distance=800),
+        IsDistancePassed(name="distance_passed_ThroughTheGate", target_distance=900),
         RunByGyro(name="run straight_SpinAndRun", target=360, power=60,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
