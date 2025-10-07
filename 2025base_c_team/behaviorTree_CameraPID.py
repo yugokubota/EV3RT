bb97b89acd0e344010021b64ef41279b9d9ecf18
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-10-07 20:10:18 +0900

    最初のよける角度調整

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -951,7 +951,7 @@ def build_behaviour_tree() -> BehaviourTree:
     obstacle_avoid_start_Parallel = Parallel(name="obstacle_avoid_start", policy=ParallelPolicy.SuccessOnOne())
     obstacle_avoid_start_Parallel.add_children([
         IsDistancePassed(name="distance_passed", target_distance=400),
-        RunByGyro(name="object_avoid_gyro", target=20, power=100,
+        RunByGyro(name="object_avoid_gyro", target=25, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
 
