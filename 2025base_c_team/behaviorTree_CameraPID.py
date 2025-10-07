4cfc16785ea70463db5224bdc229ffd1d11ca2bb
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-10-07 19:04:41 +0900

    最初のゲート通過と最後の2本目のボトル置くところの距離調整

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1168,7 +1168,7 @@ def build_behaviour_tree() -> BehaviourTree:
     BringObject_to_Gate_Parallel = Parallel(name="BringObject_to_Gate", policy=ParallelPolicy.SuccessOnOne())
     BringObject_to_Gate_Parallel.add_children([
         # -----ゲートの位置で距離が変わるようになっている⇒gate_value(300=front, 500=back)
-        IsDistancePassed(name="distance_passed", target_distance=gate_value(450, 1000)),
+        IsDistancePassed(name="distance_passed", target_distance=gate_value(450, 800)),
         RunByGyro(name="run straight_SpinAndRun", target=-160, power=60,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
@@ -1349,7 +1349,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # [Control] Parallel(SuccessOnOne)
     smart_carry_puton_second_Parallel = Parallel(name="smart_carry_puton", policy=ParallelPolicy.SuccessOnOne())
     smart_carry_puton_second_Parallel.add_children([
-        IsDistancePassed(name="smart_carry_puton", target_distance=gate_value(850, 400)),
+        IsDistancePassed(name="smart_carry_puton", target_distance=gate_value(850, 500)),
         RunByGyro(name="Gyro_straight_smart_carry_puton_second", target=180, power=60,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
