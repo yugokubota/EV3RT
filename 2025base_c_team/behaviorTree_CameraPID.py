d286eaf097ba73bba1a6cb0a3d3f707565918616
YugoKubota <MSADsystemsA2067137@outlook.jp>
2025-10-09 19:05:00 +0900

    向こう正面を2700⇒2550 最後のカーブを50⇒300

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1016,7 +1016,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # [Control] Parallel(SuccessOnOne)
     gyro_mukoujoumen_Parallel = Parallel(name="gyro_mukoujoumen", policy=ParallelPolicy.SuccessOnOne())
     gyro_mukoujoumen_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=2700),
+        IsDistancePassed(name="distance_passed", target_distance=2550),
         RunByGyro(name="gyro_mukoujoumen", target=90, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
@@ -1027,7 +1027,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # [Control] Parallel(SuccessOnOne)
     gyro_second_curve_135degree_Parallel = Parallel(name="gyro_curve_135degree", policy=ParallelPolicy.SuccessOnOne())
     gyro_second_curve_135degree_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=50),
+        IsDistancePassed(name="distance_passed", target_distance=300),
         RunByGyro(name="gyro_second_curve_135degree", target=135, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
