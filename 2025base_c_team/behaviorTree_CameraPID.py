cc6cadbe8fff6effa4a1cf2ea257981f9cefb81e
YasuyukiOda <MSADsystems6M748480@outlook.jp>
2025-10-17 19:30:44 +0900

    power40

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1379,10 +1379,10 @@ def build_behaviour_tree() -> BehaviourTree:
     place_first_seq = Sequence(name="place_first_bottle", memory=True)
     place_first_seq.add_children([
         AimBlueThenGo(name="aim_and_place",
-            base_power=35, kp_turn=0.3,
+            base_power=40, kp_turn=0.3,
             min_cy_ratio=0.60,   # 近づいた判定の高さ
             go_distance=320,     # ここを現場で調整
-            run_power=35),
+            run_power=40),
     ])
 
     # --- 最初のボトルをターゲットに置く ---
