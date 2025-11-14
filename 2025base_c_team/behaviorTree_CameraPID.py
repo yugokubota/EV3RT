b38c0512bb7784e5d9ebc91802eb32e0e147844e
YasuyukiOda <MSADsystems6M748480@outlook.jp>
2025-11-14 20:41:46 +0900

    スタート内よりでLAP-2 LAP二つ目のカーブを浅く150→135

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -934,7 +934,7 @@ def build_behaviour_tree() -> BehaviourTree:
     obstacle_Parallel.add_children([
         # avoid_seq, # 回避条件（距離到達→回避実行）
         IsDistancePassed(name="distance_passed", target_distance=2750),
-        RunByGyro(name="object_avoid_gyro", target=-4, power=100,
+        RunByGyro(name="object_avoid_gyro", target=-2, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
 
@@ -1018,7 +1018,7 @@ def build_behaviour_tree() -> BehaviourTree:
     gyro_second_curve_135degree_Parallel = Parallel(name="gyro_curve_135degree", policy=ParallelPolicy.SuccessOnOne())
     gyro_second_curve_135degree_Parallel.add_children([
         IsDistancePassed(name="distance_passed", target_distance=500),
-        RunByGyro(name="gyro_second_curve_135degree", target=-150, power=100,
+        RunByGyro(name="gyro_second_curve_135degree", target=-135, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
 
