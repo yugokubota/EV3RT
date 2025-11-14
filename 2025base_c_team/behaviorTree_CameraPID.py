18d82d219c0c8b7c1902bf91f31a4aa5827ae629
YasuyukiOda <MSADsystems6M748480@outlook.jp>
2025-11-14 20:16:35 +0900

    大円へのエッジ切り替え距離制御2200→1900

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1100,7 +1100,7 @@ def build_behaviour_tree() -> BehaviourTree:
     SmallCircle_Linetrace_InnerEdge_parallel = Parallel(name="Smallcircle_Linetrace_InnerEdge",policy=ParallelPolicy.SuccessOnOne())
     SmallCircle_Linetrace_InnerEdge_parallel.add_children([
         DetectBlue(name="detect_blue"),
-        IsDistancePassed(name="distance_passed", target_distance=2200),      #青検知しなかったとき用の距離制御
+        IsDistancePassed(name="distance_passed", target_distance=1900),      #青検知しなかったとき用の距離制御
         TraceLineCam(name="traceline_cam_inner_egde",power=43, pid_p=2.0, pid_i=0.0012, pid_d=0.1,
         gs_min=0, gs_max=50,trace_side=TraceSide.NORMAL),
     ])
@@ -1111,7 +1111,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # [Control] Parallel(SuccessOnOne)
     BigCircleEntryTuning_Parallel = Parallel(name="BigCircleEntryTuning", policy=ParallelPolicy.SuccessOnOne())
     BigCircleEntryTuning_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=350),
+        IsDistancePassed(name="distance_passed", target_distance=600),
         TraceLineCam(name="traceline_entry_bigcircle",power=42, pid_p=2.0, pid_i=0.0012, pid_d=0.1,
         gs_min=0, gs_max=80,trace_side=TraceSide.OPPOSITE),
     ])
