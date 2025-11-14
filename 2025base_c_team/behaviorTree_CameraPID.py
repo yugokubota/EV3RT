39c12694a6dd1d358e1f2197ffddccb0fca61772
T72-Nakajima <naka722.fiena@gmail.com>
2025-11-14 18:26:18 +0900

    LAP -4

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -934,7 +934,7 @@ def build_behaviour_tree() -> BehaviourTree:
     obstacle_Parallel.add_children([
         # avoid_seq, # 回避条件（距離到達→回避実行）
         IsDistancePassed(name="distance_passed", target_distance=2750),
-        RunByGyro(name="object_avoid_gyro", target=-3, power=100,
+        RunByGyro(name="object_avoid_gyro", target=-4, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
 
