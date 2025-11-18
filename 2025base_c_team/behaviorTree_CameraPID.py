53b01f66d5f8f68cb610c009ada25ab58a05902a
YasuyukiOda <MSADsystems6M748480@outlook.jp>
2025-11-18 18:55:27 +0900

    mukou 2300 →2400

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1016,7 +1016,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # [Control] Parallel(SuccessOnOne)
     gyro_mukoujoumen_Parallel = Parallel(name="gyro_mukoujoumen", policy=ParallelPolicy.SuccessOnOne())
     gyro_mukoujoumen_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=2300),
+        IsDistancePassed(name="distance_passed", target_distance=2400),
         RunByGyro(name="gyro_mukoujoumen", target=88, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
