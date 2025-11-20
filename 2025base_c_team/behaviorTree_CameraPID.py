50974e6a07b9f19ad693e55e7d787f7603ca9a74
YasuyukiOda <MSADsystems6M748480@outlook.jp>
2025-11-20 09:43:46 +0900

    2カーブ500→400

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -1030,7 +1030,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # [Control] Parallel(SuccessOnOne)
     gyro_second_curve_135degree_Parallel = Parallel(name="gyro_curve_135degree", policy=ParallelPolicy.SuccessOnOne())
     gyro_second_curve_135degree_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=500),
+        IsDistancePassed(name="distance_passed", target_distance=400),
         RunByGyro(name="gyro_second_curve_135degree", target=130, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
