869ea978d572054cffd341acee9d948c1fc66f53
YasuyukiOda <MSADsystems6M748480@outlook.jp>
2025-11-20 12:14:02 +0900

    leftと上がり幅を合わせた

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -933,7 +933,7 @@ def build_behaviour_tree() -> BehaviourTree:
     obstacle_Parallel = Parallel(name="obstacle_or_gyro", policy=ParallelPolicy.SuccessOnOne())
     obstacle_Parallel.add_children([
         # avoid_seq, # 回避条件（距離到達→回避実行）
-        IsDistancePassed(name="distance_passed", target_distance=2750),
+        IsDistancePassed(name="distance_passed", target_distance=2450),
         RunByGyro(name="object_avoid_gyro", target=-2, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
@@ -948,7 +948,7 @@ def build_behaviour_tree() -> BehaviourTree:
 
     obstacle_avoid_middle_Parallel = Parallel(name="obstacle_avoid_middle", policy=ParallelPolicy.SuccessOnOne())
     obstacle_avoid_middle_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=850),
+        IsDistancePassed(name="distance_passed", target_distance=1150),
         RunByGyro(name="object_avoid_gyro", target=5, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
@@ -1008,7 +1008,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # [Control] Parallel(SuccessOnOne)
     gyro_mukoujoumen_Parallel = Parallel(name="gyro_mukoujoumen", policy=ParallelPolicy.SuccessOnOne())
     gyro_mukoujoumen_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=2700),
+        IsDistancePassed(name="distance_passed", target_distance=2800),
         RunByGyro(name="gyro_mukoujoumen", target=-84, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
