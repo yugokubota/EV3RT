f541e64e25aaf0e76d67f0bf173687c3fb5ef925
YugoKubota <MSADsystemsA2067137@outlook.jp>
2026-01-23 18:15:01 +0900

    obstacle_avoid_start_Parallelの距離を500から450に修正

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -927,7 +927,7 @@ def build_behaviour_tree() -> BehaviourTree:
 
     obstacle_avoid_start_Parallel = Parallel(name="obstacle_avoid_start", policy=ParallelPolicy.SuccessOnOne())
     obstacle_avoid_start_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=500),
+        IsDistancePassed(name="distance_passed", target_distance=450),
         RunByGyro(name="object_avoid_gyro", target=30, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
