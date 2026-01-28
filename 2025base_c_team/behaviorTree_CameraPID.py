fcc6e70de18e8e13f52c41526604f7784a60a901
YasuyukiOda <MSADsystems6M748480@outlook.jp>
2026-01-28 12:57:36 +0900

    お試し

--- 2025base_c_team/behaviorTree_CameraPID.py
+++ 2025base_c_team/behaviorTree_CameraPID.py
@@ -927,14 +927,14 @@ def build_behaviour_tree() -> BehaviourTree:
 
     obstacle_avoid_start_Parallel = Parallel(name="obstacle_avoid_start", policy=ParallelPolicy.SuccessOnOne())
     obstacle_avoid_start_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=550),
+        IsDistancePassed(name="distance_passed", target_distance=500),
         RunByGyro(name="object_avoid_gyro", target=-30, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
 
     obstacle_avoid_middle_Parallel = Parallel(name="obstacle_avoid_middle", policy=ParallelPolicy.SuccessOnOne())
     obstacle_avoid_middle_Parallel.add_children([
-        IsDistancePassed(name="distance_passed", target_distance=400),
+        IsDistancePassed(name="distance_passed", target_distance=800),
         RunByGyro(name="object_avoid_gyro", target=0, power=100,
                 pid_p=1.1, pid_i=0.001, pid_d=0.03, target_type=HeadingType.ABSOLUTE),
     ])
@@ -1434,7 +1434,7 @@ def build_behaviour_tree() -> BehaviourTree:
         obstacle_Parallel,
         obstacle_avoid_start_Parallel,
         obstacle_avoid_middle_Parallel,
-        obstacle_avoid_end_Parallel,
+        # obstacle_avoid_end_Parallel,
         # --- 一定距離走行⇒カーブを曲がる処理⇒向正面走行⇒カーブを曲がる処理⇒LAPまで直進
         gyro_obstacle_end_to_first_curve_Parallel,
         gyro_first_curve_45degree_Parallel,
